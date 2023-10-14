193b6760a7530bd1e0e515554b97156c613ae112
tywest121 <tyresewest25@gmail.com>
2023-10-14 01:31:04 -0400

    Xecell

---
+++ app.js
@@ -0,0 +1,28 @@
+var a= 15
+var b= 30
+
+
+function teddybear(num1,num2){
+    return num1 + num2
+}
+console.log(teddybear(a,b))
+
+function angel(num1,num2){
+    return num1 - num2
+}
+console.log(angel(a,b))
+
+function superman(param1,param2){
+    return param1 * param2
+}
+console.log(superman(a,b))
+
+function batman(param1,param2){
+    return param1 / param2
+}
+console.log(batman(a,b))
+
+let myFunction = (a, b) => a * b;
+let noFunction = (a, b) => a / b;
+let yesFunction = (a, b) => a + b;
+let rowFunction = (a, b) => a - b;
